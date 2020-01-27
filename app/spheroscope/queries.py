73c05dcd922bab463becc51a9813b7c538d1f59c
Philipp Heinrich <philipp.heinrich@fau.de>
2020-01-27 13:18:50 +0100

    visualize fillform

--- app/spheroscope/queries.py
+++ app/spheroscope/queries.py
@@ -6,12 +6,12 @@ from werkzeug.exceptions import abort
 from spheroscope.auth import login_required
 from spheroscope.db import get_db
 
-import pandas as pd
 import gzip
 import json
 import os
-from collections import Counter
 import subprocess
+
+from .format_utils import format_query_result
 
 bp = Blueprint('queries', __name__, url_prefix='/queries')
 
@@ -113,85 +113,6 @@ def update(id):
     return render_template('queries/update.html', query=query)
 
 
-def format_query_result(query_result):
-
-    # init result
-    result = dict()
-    result['query'] = query_result['query']
-    result['pattern'] = query_result['pattern']
-
-    # TODO make consistent
-    if 'name' in query_result.keys():
-        result['title'] = query_result['name']
-    else:
-        result['title'] = query_result['title']
-
-    # format anchors
-    anchors = pd.DataFrame(query_result['anchors'])
-    anchors.columns = ['number', 'correction', 'hole', 'clear name']
-    result['anchors'] = anchors.to_html(escape=False, index=False)
-
-    # format regions
-    regions = pd.DataFrame(query_result['regions'])
-    if not regions.empty:
-        regions.columns = ['start', 'end', 'hole', 'clear name']
-        result['regions'] = regions.to_html(escape=False, index=False)
-    else:
-        result['regions'] = None
-
-    # get matches
-    if 'matches' in query_result['result'].keys():
-        result['matches'] = list()
-        for key in query_result['result']['matches'].keys():
-
-            # get holes
-            holes = query_result['result']['matches'][key].pop('holes')
-
-            # get rid of the offset
-            query_result['result']['matches'][key].pop('offset')
-
-            # format anchor column
-            df = pd.DataFrame(query_result['result']['matches'][key], dtype=str)
-            df.fillna(-1, inplace=True)
-            df['anchor'] = df['anchor'].apply(pd.to_numeric, downcast='integer')
-            df = df.replace(-1, "")
-
-            # format match column
-            df['match'] = df['match'].replace("False", "")
-            df['match'] = df['match'].replace("True", "match")
-
-            seq = " ".join(
-                query_result['result']['matches'][key]['word'].values()
-            )
-            line = {
-                'full': seq,
-                'position': key,
-                'holes': holes,
-                'df': df.to_html(
-                    escape=False, index_names='cpos', bold_rows=False
-                )
-            }
-            result['matches'].append(line)
-        result['nr_matches'] = query_result['result']['nr_matches']
-    else:
-        result['matches'] = list()
-        result['nr_matches'] = 0
-
-    # frequency count of holes
-    counts = dict()
-    for key in query_result['result']['holes'].keys():
-        df = pd.DataFrame.from_dict(
-            Counter(query_result['result']['holes'][key]), orient='index'
-        ).sort_values(by=0, ascending=False)
-        df.columns = ['freq']
-        df.index.name = key
-        counts[key] = df.to_html(escape=False, index_names=True, bold_rows=False)
-    result['holes'] = counts
-
-    # return
-    return result
-
-
 @bp.route('/<int:id>/show_result', methods=('GET', 'POST'))
 @login_required
 def show_result(id):
@@ -243,22 +164,23 @@ def run(id):
 
     # init CWBEngine
     from ccc.cwb import CWBEngine
-    from ccc.anchors import anchor_query
+    from ccc.argmin import anchor_query
     engine = CWBEngine(
         corpus_name=current_app.config['CORPUS_NAME'],
         lib_path=current_app.config['LIB_PATH'],
-        registry_path=current_app.config['REGISTRY_PATH']
+        registry_path=current_app.config['REGISTRY_PATH'],
+        cache_path=current_app.config['CACHE_PATH']
     )
 
     # run person_any once
     print(engine.cqp.Exec("/person_any[];"))
 
     # restrict to subcorpus
-    # subcorpus = (
-    #     "DEDUP=/region[tweet,a] :: (a.tweet_duplicate_status!='1') within tweet;"
-    #     "DEDUP;"
-    # )
-    # engine.cqp.Exec(subcorpus)
+    subcorpus = (
+        "DEDUP=/region[tweet,a] :: (a.tweet_duplicate_status!='1') within tweet;"
+        "DEDUP;"
+    )
+    engine.cqp.Exec(subcorpus)
 
     # set concordance settings
     concordance_settings = {
@@ -272,13 +194,15 @@ def run(id):
     query['concordance_settings'] = concordance_settings
 
     # create result
-    query['result'] = anchor_query(engine,
-                                   query['query'],
-                                   query['anchors'],
-                                   query['regions'],
-                                   concordance_settings['s_break'],
-                                   concordance_settings['p_show'],
-                                   concordance_settings['match_strategy'])
+    query['result'] = anchor_query(
+        engine,
+        query_str=query['query'],
+        anchors=query['anchors'],
+        regions=query['regions'],
+        s_break=concordance_settings['s_break'],
+        p_show=concordance_settings['p_show'],
+        match_strategy=concordance_settings['match_strategy']
+    )
 
     # dump result
     with gzip.open(path_result, 'wt') as f_out:
