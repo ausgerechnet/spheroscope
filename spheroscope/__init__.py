67390b7e8389b5a6836fe9982422e801730aaf02
Philipp Heinrich <philipp.heinrich@fau.de>
2020-06-21 03:54:05 +0200

    some doc

--- spheroscope/__init__.py
+++ spheroscope/__init__.py
@@ -1,22 +1,27 @@
 import os
-# flask
 from flask import Flask
 
 
 def create_app(test_config=None):
 
     # create and configure the app
-    app = Flask(__name__, instance_relative_config=True)
+    app = Flask('spheroscope', instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY='dev',
         DATABASE=os.path.join(app.instance_path, 'spheroscope.sqlite'),
     )
 
+    # read configuration if not testing
     if test_config is None:
-        # load the instance config, if it exists, when not testing
-        app.config.from_pyfile('spheroscope.cfg', silent=True)
+        app.config.from_pyfile('../spheroscope.cfg')
+
+        # TODO: move corpus choice to interface
+        corpus_config = os.path.join('..', 'library',
+                                     app.config['CORPUS'],
+                                     app.config['CORPUS'] + '.cfg')
+        app.config.from_pyfile(corpus_config)
+
     else:
-        # load the test config if passed in
         app.config.from_mapping(test_config)
 
     # ensure the instance folder exists
@@ -25,29 +30,37 @@ def create_app(test_config=None):
     except OSError:
         pass
 
-    # just for testing
+    # say hello
     @app.route('/hello')
     def hello():
-        return 'Hello, World!'
+        return 'Hello back there!'
 
+    # initialize database
     from . import db
     db.init_app(app)
 
+    # authentication
     from . import auth
     app.register_blueprint(auth.bp)
 
+    # index
     from . import index
     app.register_blueprint(index.bp)
     app.add_url_rule('/', endpoint='index')
 
+    # wordlists
     from . import wordlists
     app.register_blueprint(wordlists.bp)
 
+    # patterns
     from . import patterns
     app.register_blueprint(patterns.bp)
 
+    # queries
     from . import queries
     app.register_blueprint(queries.bp)
+
+    # add run-queries command
     queries.add_run_queries(app)
 
     return app
