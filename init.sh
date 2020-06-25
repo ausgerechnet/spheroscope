da15a75e9bd24e0ee91e67c4be982d23abf864d5
Philipp Heinrich <philipp.heinrich@fau.de>
2020-06-25 10:05:18 +0200

    tests

---
+++ init.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+export FLASK_APP=spheroscope
+export FLASK_ENV=development
+flask init-db
+flask import-lib
--- tests/test_corpora.py
+++
@@ -1,7 +0,0 @@
-from spheroscope.corpora impor init_corpus
-
-
-config = 
-
-def test_init_corpus():
-    init_corpus(
