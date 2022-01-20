80cc8c47e2c9bc701e52c61b91fe944b7be14276
Philipp Heinrich <philipp.heinrich@fau.de>
2022-01-20 12:14:26 +0100

    streamline pattern matches and hierarchical queries; add gold evaluation

--- spheroscope/patterns.py
+++ spheroscope/patterns.py
@@ -9,34 +9,37 @@ from .auth import login_required
 from .database import Pattern, Query
 from .corpora import read_config, init_corpus
 
-from pandas import Series, DataFrame, concat, read_csv
+from pandas import concat, read_csv
 
 bp = Blueprint('patterns', __name__, url_prefix='/patterns')
 
 
-def run_queries_(queries, cwb_id):
+def run_queries(queries, cwb_id):
+    """collect matches of all queries belonging to one pattern as
+    dataframe.
+
+    index:
+    - <str> query_name: name of the query
+    - <int> match: match position of query
+    - <int> matchend: matchend position of query
+
+    columns:
+    - corpus_config['display']['p_show']: p-attribute realization sequences of context
+    - corpus_config['display']['s_show']: s-attribute realizations at match
+    - for each slot in query['anchors']['slots']:
+       - slot_START: start position of slot
+       - slot_END: end position of slot
+       for each p-att:
+         - slot_p-att: p-attribute realization sequence of slot
+
     """
-    collect matches of all queries belonging to one pattern as dataframe
 
-    query_name
-    match
-    matchend
-    corpus_config['display']['p_show']: p-attribute realization sequences of context
-    corpus_config['display']['s_show']: s-attribute realizations at match
-    for each slot in query['anchors']['slots']:
-      slot_START
-      slot_END
-      for each p-att:
-        slot_p-att
-
-    """
-
+    # init corpus
     corpus_config = read_config(cwb_id)
     corpus = init_corpus(corpus_config)
 
     # run all queries belonging to a pattern
     matches_list = list()
-
     for query in queries:
 
         current_app.logger.info("query: %s", query.name)
@@ -76,15 +79,19 @@ def run_queries_(queries, cwb_id):
                               "_".join([str(slot), 'END'])]
             matches = matches.join(df)
 
+        # append to result list
         matches_list.append(matches.reset_index())
 
+    # create output dataframe
     result = concat(matches_list).set_index(['query', 'match', 'matchend'])
 
     return result
 
 
 def add_gold(result, cwb_id, pattern, s_cwb='tweet_id', s_gold='tweet'):
+    """add gold annotation to result of run_queries()
 
+    """
     try:
         gold = read_csv(
             os.path.join("library", cwb_id, "gold", "adjudicated.tsv"),
@@ -93,22 +100,20 @@ def add_gold(result, cwb_id, pattern, s_cwb='tweet_id', s_gold='tweet'):
     except FileNotFoundError:
         result['TP'] = None
     else:
-
         # pre-process gold
-        gold = gold.loc[
-            gold['pattern'] == pattern
-        ].rename(
+        gold = gold.loc[gold['pattern'] == pattern].rename(
             {s_gold: s_cwb, 'annotation': 'TP'}, axis=1
         )
-
         # join explicit TPs and FPs
+        result = result.reset_index()
         result = result.merge(gold[[s_cwb, "TP"]], on=s_cwb, how='left')
+        result = result.set_index(['query', 'match', 'matchend'])
 
     return result
 
 
 def evaluate(tps):
-    """
+    """evaluate "TP" column of result of run_queries()
 
     :param pd.Series tps: column with True / False annotations
 
@@ -129,77 +134,38 @@ def evaluate(tps):
     return tps
 
 
-def run_queries(queries, cwb_id, slot):
+def create_subcorpus(df, slot):
+    """transform one slot of result of run_queries() into a valid dump
+    (empty dataframe multi-indexed by match and matchend)
 
-    corpus_config = read_config(cwb_id)
-    corpus = init_corpus(corpus_config)
+    """
 
-    # run all queries belonging to a pattern
-    dfs = list()
-    concs = list()
+    # select appropriate columns
+    column_start = "_".join([str(slot), "START"])
+    column_end = "_".join([str(slot), "END"])
+    dump = df.reset_index(drop=True)[[column_start, column_end]]
 
-    for query in queries:
+    # only take rows where there's a match, sort ascending and deduplicate
+    dump = dump.fillna(-1, downcast='infer')
+    dump = dump[dump[column_start] != -1]
+    dump = dump.sort_values(by=column_start)
+    dump = dump.drop_duplicates()
 
-        current_app.logger.info("query: %s", query.name)
-        query = query.serialize()
+    # post-proc: behaviour when start > end
+    dump[column_end][
+        dump[column_start] > dump[column_end]
+    ] = dump[column_start][
+        dump[column_start] <= dump[column_end]
+    ]
 
-        # get dump
-        dump = corpus.query(
-            cqp_query=query['cqp'],
-            context=corpus_config['query']['context'],
-            context_break=corpus_config['query']['context_break'],
-            corrections=query['anchors']['corrections'],
-            match_strategy=corpus_config['query']['match_strategy']
-        )
-
-        # process slots
-        try:
-            columns = query['anchors']['slots'][slot]
-        except KeyError:
-            current_app.logger.error(
-                "undefined slot in query %s" % query['meta']['name']
-            )
-        else:
-
-            # for subcorpus creation
-            df = dump.df[columns]
-            if isinstance(df, Series):
-                df = DataFrame(df)
-                df.columns = ['start']
-            if df.shape[1] == 1:
-                df.columns = ['start']
-                df['end'] = df['start']
-            elif df.shape[1] == 2:
-                df.columns = ['start', 'end']
-            dfs.append(df)
-
-            # for concordance
-            conc = dump.concordance(
-                form='slots',
-                p_show=dict(corpus_config['display'])['p_show'],
-                s_show=dict(corpus_config['display'])['s_show'],
-                cut_off=None,
-                slots=query['anchors']['slots']
-            )
-            concs.append(conc)
-
-    # create subcorpus
-    df = concat(dfs)
-    df = df.reset_index(drop=True)
-    df = df[df.start != -1]
-    df = df.sort_values(by='start')
-    df = df.drop_duplicates()
-    df['end'][df['start'] > df['end']] = df['start'][df['start'] <= df['end']]
-    df = df.rename(columns={'start': 'match', 'end': 'matchend'}).set_index(
+    # rename, convert to int, set index
+    dump = dump.rename(
+        columns={column_start: 'match', column_end: 'matchend'}
+    ).set_index(
         ['match', 'matchend']
     )
 
-    # create full concordance
-    conc = concat(concs).reset_index().set_index(
-        dict(corpus_config['display'])['s_show'][0]
-    )
-
-    return df, conc
+    return dump
 
 
 ######################################################
@@ -245,7 +211,8 @@ def matches(id):
 
     cwb_id = session['corpus']['resources']['cwb_id']
     queries = Query.query.filter_by(pattern_id=id).order_by(Query.name).all()
-    matches = run_queries_(queries, cwb_id)
+    matches = run_queries(queries, cwb_id)
+
     matches = add_gold(matches, cwb_id, id)
     tps = evaluate(matches['TP'])
     matches = matches.sample(cut_off)
@@ -255,9 +222,9 @@ def matches(id):
                            tps=tps)
 
 
-@bp.route('/<int(signed=True):id>/subquery', methods=('GET', 'POST'))
+@bp.route('/<int(signed=True):id>/matches/subquery', methods=('GET', 'POST'))
 @login_required
-def run_subquery(id):
+def subquery(id):
     """execute a hierarchical query: retrieve matches of all queries
     belonging to a _base_ pattern, then run all queries belonging to
     _slot_ pattern on one slot defined in the base pattern.
@@ -288,16 +255,21 @@ def run_subquery(id):
     slot_pattern = request.args.get('slot_pattern')
     slot_queries = Query.query.filter_by(pattern_id=slot_pattern).all()
 
-    # load corpus
+    # run all queries belonging to base pattern
+    matches = run_queries(base_queries, cwb_id)
+    corpus_config = read_config(cwb_id)
+    matches = matches.reset_index().set_index(
+        dict(corpus_config['display'])['s_show'][0]
+    )
+
+    # activate NQR
+    df_dump = create_subcorpus(matches, slot)
     corpus_config = read_config(cwb_id)
     corpus = init_corpus(corpus_config)
     name = "Pattern%dSlot%s" % (id, slot)
-    df, conc = run_queries(base_queries, cwb_id, slot)
+    corpus.activate_subcorpus(name, df_dump)
 
-    # activate NQR
-    corpus.activate_subcorpus(name, df)
-
-    # run all queries belonging to slot pattern on subcorpus
+    # run all queries belonging to slot pattern on activated NQR
     concs = list()
     for query in slot_queries:
 
@@ -310,16 +282,19 @@ def run_subquery(id):
             corrections=query['anchors']['corrections'],
             match_strategy=corpus_config['query']['match_strategy']
         )
-        concs.append(dump.concordance(
+        conc = dump.concordance(
             form='slots',
             p_show=dict(corpus_config['display'])['p_show'],
             s_show=dict(corpus_config['display'])['s_show'],
             cut_off=None,
             slots=query['anchors']['slots']
-        ))
+        )
+        conc['name'] = query['meta']['name']
+        concs.append(conc)
 
     # join
     conc_slot = concat(concs).reset_index()
+    # post-process
     if len(conc_slot) > 0:
         d = conc_slot[
             ["_".join([s, p]) for p in corpus_config['display']['p_show']
@@ -334,12 +309,13 @@ def run_subquery(id):
         d = d.rename(columns=renames).set_index(
             dict(corpus_config['display'])['s_show'][0]
         )
-        result = conc.join(d, how='inner')
+        result = matches.join(d, how='inner')
     else:
-        result = conc
+        result = matches
 
-    # dummy
-    tps = {'TP': "nan", 'FP': "nan", 'prec': "nan", 'rec': "nan"}
+    # evaluate matches on slot
+    result = add_gold(result, cwb_id, slot)
+    tps = evaluate(result['TP'])
 
     return render_template('queries/result_table.html',
                            result=result,
--- tests/test_patterns.py
+++ tests/test_patterns.py
@@ -33,7 +33,7 @@ def test_hierarchy_3_1_5(client, auth):
         slot_pattern = 5
 
         response = client.get(
-            "https://localhost/patterns/{base_pattern}/"
+            "https://localhost/patterns/{base_pattern}/matches/"
             "subquery?slot={slot}&slot_pattern={slot_pattern}".format(
                 base_pattern=base_pattern, slot=slot, slot_pattern=slot_pattern
             )
@@ -49,14 +49,14 @@ def test_hierarchy_0_1_3(client, auth):
 
         client.get("/")         # initialize session
 
-        # http://127.0.0.1:5000/patterns/0/subquery?slot=1&slot_pattern=3
+        # http://127.0.0.1:5000/patterns/0/matches/subquery?slot=1&slot_pattern=3
         # base = quotation, slot = formula, slot_pattern = desire
         base_pattern = 0
         slot = 1
         slot_pattern = 3
 
         response = client.get(
-            "https://localhost/patterns/{base_pattern}/"
+            "https://localhost/patterns/{base_pattern}/matches/"
             "subquery?slot={slot}&slot_pattern={slot_pattern}".format(
                 base_pattern=base_pattern, slot=slot, slot_pattern=slot_pattern
             )
@@ -72,14 +72,14 @@ def test_hierarchy_3_0_24(client, auth):
 
         client.get("/")         # initialize session
 
-        # http://127.0.0.1:5000/patterns/3/subquery?slot=0&slot_pattern=24
+        # http://127.0.0.1:5000/patterns/3/matches/subquery?slot=0&slot_pattern=24
         # base = desire, slot = entity, slot_pattern = membership
         base_pattern = 3
         slot = 0
         slot_pattern = 24
 
         response = client.get(
-            "https://localhost/patterns/{base_pattern}/"
+            "https://localhost/patterns/{base_pattern}/matches/"
             "subquery?slot={slot}&slot_pattern={slot_pattern}".format(
                 base_pattern=base_pattern, slot=slot, slot_pattern=slot_pattern
             )
