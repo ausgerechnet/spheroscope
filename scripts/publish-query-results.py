9315966c97d3cacfa0ef1c4e41108253bad2ba57
Philipp Heinrich <philipp.heinrich@fau.de>
2022-02-28 19:54:55 +0100

    sort gold & query results; add results pattern 24

--- scripts/publish-query-results.py
+++ scripts/publish-query-results.py
@@ -1,8 +1,10 @@
 from glob import glob
 from pandas import read_csv, DataFrame, concat
+from argparse import ArgumentParser
 
 
 def main(glob_in):
+
     paths = glob(glob_in)
     results = list()
     for p in paths:
@@ -15,16 +17,25 @@ def main(glob_in):
         results.append(df)
     df = concat(results)
     df.index.name = 'tweet_id'
+
     return df
 
 
 if __name__ == '__main__':
+
+    parser = ArgumentParser()
+    parser.add_argument('pattern',
+                        type=str)
+    args = parser.parse_args()
+
     glob_in = (
         "instance/BREXIT_V20190522_DEDUP/results/"
-        "pattern3_*.tsv"
+        "pattern%s_*.tsv" % args.pattern
     )
+
     df = main(glob_in)
+
     df.to_csv(
-        'library/BREXIT_V20190522_DEDUP/gold/'
-        'pattern3-query-results.tsv', sep="\t"
+        'library/BREXIT_V20190522_DEDUP/query-results/'
+        'pattern%s-query-results.tsv' % args.pattern, sep="\t"
     )
