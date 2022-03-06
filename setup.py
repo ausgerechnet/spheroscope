574d2cc285fc93ca357c5abe242cf27440ab23b2
Philipp Heinrich <philipp.heinrich@fau.de>
2022-03-06 04:16:52 +0100

    update setup.py; use stable cwb-ccc v0.10.2

--- setup.py
+++ setup.py
@@ -23,19 +23,10 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/ausgerechnet/spheroscope",
     install_requires=[
-        "Flask>=1.1.2,<1.2",
-        "Flask-Migrate>=2.3.1,<2.8",
-        "Flask-Script>=2.0.6,<2.1",
-        "Flask-SQLAlchemy>=2.5.1,<2.6",
-        "Flask-WTF>=0.14.3,<0.15",
-        "Flask-User>=1.0.2.2,<1.1",
-        "Flask-JWT-Extended>=4.1.0,<4.2",
-        "Flask-Caching>=1.10.1,<1.11",
-        "Flask-Cors>=3.0.7,<3.1",
-        "flask-expects-json>=1.5.0,<1.6",
-        "gevent>=1.4.0,<20.12",
-        "pymagnitude>=0.1.120,<0.2",
-        "cwb-ccc>=0.10.0,<0.11"
+        "Flask>=2.0,<2.1",
+        "Flask-SQLAlchemy>=2.5,<2.6",
+        "pymagnitude>=0.1.140,<0.2",
+        "cwb-ccc==0.10.2"
     ],
     classifiers=[
         "Development Status :: 3 - Alpha",
