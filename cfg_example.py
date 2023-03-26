2b63c8b63bb3618d7e24e9395d84537200315537
Philipp Heinrich <philipp.heinrich@fau.de>
2023-03-26 10:05:08 +0200

    multiprocessing

--- cfg_example.py
+++ cfg_example.py
@@ -1,9 +1,13 @@
+from multiprocessing import cpu_count
+
+
 class DevConfig:
 
     DEBUG = False               # show interactive debugger?
     TESTING = False             # propagate exceptions?
     SECRET_KEY = "dev"          # change in production!
     SESSION_COOKIE_SECURE = False  # only send cookies via https? (disable for development server)
+    NR_CPUS = int(cpu_count() / 2) - 1  # number of CPUs to use when multiprocessing
 
     DB_NAME = "spheroscope.sqlite"  # path to database (relative to instance folder)
     DB_USERNAME = "admin"           # local database username
@@ -16,4 +20,4 @@ class DevConfig:
     REGISTRY_PATH = "/usr/local/share/cwb/registry/"  # path to your CWB registry
     CACHE_PATH = "/tmp/spheroscope-cache"  # path for CQP dumps and cwb-ccc data
 
-    EMBEDDINGS = {}
+    EMBEDDINGS = {}             # path to embeddings (cwb_id:path)
